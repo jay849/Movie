44e256bbcd7f23c487de44ea910634c202140cd2
jay <yjayp927@gmail.com>
2024-03-21 11:17:55 +0900

    d0321 1st

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Button from "./Button.js"
-import styles from "./App.module.css"
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((prev) => prev + 1);
+    console.log("I run all the time");
+    const iRunOnlyOnce = () => {
+        console.log("I run only once!");
+    };
+    useEffect(iRunOnlyOnce, []);
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
